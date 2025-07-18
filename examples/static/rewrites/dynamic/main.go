f04d6a773688126ef597408ccd6d5b7b901b6509
Sam <sktylr@gmail.com>
2025-07-19 00:25:27 +0100

    add dynamic rewrite example

---
+++ examples/static/rewrites/dynamic/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sktylr/routeit"
+)
+
+type ContactForm struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+}
+
+func GetServer() *routeit.Server {
+	srv := routeit.NewServer(routeit.ServerConfig{
+		StaticDir:      "assets",
+		URLRewritePath: "conf/rewrites.conf",
+	})
+	srv.RegisterRoutesUnderNamespace("/api", routeit.RouteRegistry{
+		"/contact": routeit.Post(func(rw *routeit.ResponseWriter, req *routeit.Request) error {
+			var in ContactForm
+			err := req.BodyToJson(&in)
+			if err != nil {
+				return err
+			}
+
+			// In reality we should never ever log a user's email or name (or
+			// any free form entered text) 😅
+			fmt.Printf("Received contact from [%s, %s]: %#q\n", in.Name, in.Email, in.Message)
+
+			rw.Textf("Thanks for your message %s!", in.Name)
+			return nil
+		}),
+	})
+	return srv
+}
+
+func main() {
+	GetServer().StartOrPanic()
+}

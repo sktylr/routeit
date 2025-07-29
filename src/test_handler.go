b3b804a1b07d796e8c1cfd40f6cc67a56e81a83e
Sam <sktylr@gmail.com>
2025-07-29 22:30:43 +0100

    allow path params to be defined when testing handlers or middleware

--- src/test_handler.go
+++ src/test_handler.go
@@ -5,9 +5,6 @@ package routeit
 // take whatever headers, body and context the test needs to test the handler
 // appropriately. Under test circumstances, the handler will **not** reject the
 // request if the URI does not match the URI the handler is registered to.
-// Currently this does not allow path parameters (e.g. those that would be
-// extracted from dynamic path components) to be configured, but does allow
-// query parameters to be configured using the path.
 func TestHandler(h Handler, tr *TestRequest) (*TestResponse, error) {
 	rw := newResponseForMethod(tr.req.Method())
 	err := h.handle(rw, tr.req)

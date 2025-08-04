58885b319f0fce8a33b4e32687df4a2492b2f60d
Sam <sktylr@gmail.com>
2025-08-04 17:46:22 +0100

    verify headers writing using multiple header values

--- src/headers_test.go
+++ src/headers_test.go
@@ -111,50 +111,60 @@ func TestHeadersFromRaw(t *testing.T) {
 func TestHeadersWriteTo(t *testing.T) {
 	tests := []struct {
 		name string
-		in   map[string]string
+		in   map[string][]string
 		want string
 	}{
 		{
-			"multiple multi-case headers",
-			map[string]string{
-				"Content-Type":   "application/json",
-				"date":           "Fri, 13 Jun 2025 19:35:42 GMT",
-				"Content-length": "85",
+			name: "multiple multi-case headers",
+			in: map[string][]string{
+				"Content-Type":   {"application/json"},
+				"date":           {"Fri, 13 Jun 2025 19:35:42 GMT"},
+				"Content-length": {"85"},
 			},
-			"Content-Type: application/json\r\ndate: Fri, 13 Jun 2025 19:35:42 GMT\r\nContent-length: 85\r\n",
+			want: "Content-Type: application/json\r\ndate: Fri, 13 Jun 2025 19:35:42 GMT\r\nContent-length: 85\r\n",
 		},
 		{
-			"empty",
-			map[string]string{},
-			"",
+			name: "empty",
 		},
 		{
-			"clears new lines",
-			map[string]string{"Content-Length": "1\n5\n"},
-			"Content-Length: 15\r\n",
+			name: "clears new lines",
+			in:   map[string][]string{"Content-Length": {"1\n5\n"}},
+			want: "Content-Length: 15\r\n",
 		},
 		{
-			"clears internal carriage return",
-			map[string]string{"Content-Type": "application/\r\njson"},
-			"Content-Type: application/json\r\n",
+			name: "clears internal carriage return",
+			in:   map[string][]string{"Content-Type": {"application/\r\njson"}},
+			want: "Content-Type: application/json\r\n",
 		},
 		{
-			"does not clear internal whitespace",
-			map[string]string{"Content-Type": "application/json; charset=utf-8"},
-			"Content-Type: application/json; charset=utf-8\r\n",
+			name: "does not clear internal whitespace",
+			in:   map[string][]string{"Content-Type": {"application/json; charset=utf-8"}},
+			want: "Content-Type: application/json; charset=utf-8\r\n",
 		},
 		{
-			"allows HTAB",
-			map[string]string{"Content-Type": "application/json\tcharset=utf-8"},
-			"Content-Type: application/json\tcharset=utf-8\r\n",
+			name: "allows HTAB",
+			in:   map[string][]string{"Content-Type": {"application/json\tcharset=utf-8"}},
+			want: "Content-Type: application/json\tcharset=utf-8\r\n",
+		},
+		{
+			name: "handles multiple header values",
+			in:   map[string][]string{"Allow": {"GET", "PUT", "POST"}},
+			want: "Allow: GET\r\nAllow: PUT\r\nAllow: POST\r\n",
+		},
+		{
+			name: "handles complex multiple header values",
+			in:   map[string][]string{"Allow": {"GET,HEAD", "PUT,PATCH", "POST,DELETE,OPTIONS"}},
+			want: "Allow: GET,HEAD\r\nAllow: PUT,PATCH\r\nAllow: POST,DELETE,OPTIONS\r\n",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			h := headers{}
-			for k, v := range tc.in {
-				h.Set(k, v)
+			for k, vals := range tc.in {
+				for _, v := range vals {
+					h.Append(k, v)
+				}
 			}
 
 			var sb strings.Builder

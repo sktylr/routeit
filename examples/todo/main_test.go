25badb35cff1ab3088423d643d0930db01de23f1
Sam <sktylr@gmail.com>
2025-08-05 20:09:23 +0100

    add simple frontend tests

--- examples/todo/main_test.go
+++ examples/todo/main_test.go
@@ -1,1 +1,83 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sktylr/routeit"
+)
+
+func TestFrontend(t *testing.T) {
+	tests := []struct {
+		endpoints    []string
+		wantCT       string
+		wantCLen     int
+		wantStart    string
+		wantContains []string
+	}{
+		{
+			endpoints:    []string{"/static/html/login.html", "/login"},
+			wantCT:       "text/html; charset=utf-8",
+			wantCLen:     1546,
+			wantStart:    "<!DOCTYPE html>",
+			wantContains: []string{"<title>Login</title>", "Don't have an account?\n      <a href=\"/register\">Register here</a>"},
+		},
+		{
+			endpoints:    []string{"/static/html/register.html", "/register"},
+			wantCT:       "text/html; charset=utf-8",
+			wantCLen:     1916,
+			wantStart:    "<!DOCTYPE html>",
+			wantContains: []string{"<title>Register</title>", "Already have an account?\n      <a href=\"/login\">Login here</a>"},
+		},
+		{
+			endpoints: []string{"/static/styles/base.css", "/css/base.css"},
+			wantCT:    "text/css; charset=utf-8",
+			wantCLen:  347,
+			wantStart: "* {\n  box-sizing: border-box;",
+			wantContains: []string{
+				`font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;`,
+				"a {\n  color: #007bff;\n  text-decoration: none;\n}",
+			},
+		},
+		{
+			endpoints: []string{"/static/styles/auth.css", "/css/auth.css"},
+			wantCT:    "text/css; charset=utf-8",
+			wantCLen:  1095,
+			wantStart: "form {\n  background-color: white;",
+			wantContains: []string{
+				"input:focus {\n  border-color: #007bff;\n  outline: none;\n  background: #f0f8ff;\n}",
+			},
+		},
+	}
+	client := routeit.NewTestClient(GetFrontendServer())
+
+	for _, tc := range tests {
+		for _, ep := range tc.endpoints {
+			t.Run(ep, func(t *testing.T) {
+				verifyMeta := func(t *testing.T, res *routeit.TestResponse) {
+					t.Helper()
+					res.AssertStatusCode(t, routeit.StatusOK)
+					res.AssertHeaderMatchesString(t, "Content-Type", tc.wantCT)
+					res.AssertHeaderMatchesString(t, "Content-Length", fmt.Sprintf("%d", tc.wantCLen))
+				}
+
+				t.Run("GET", func(t *testing.T) {
+					res := client.Get(ep)
+
+					verifyMeta(t, res)
+					res.AssertBodyStartsWithString(t, tc.wantStart)
+					for _, want := range tc.wantContains {
+						res.AssertBodyContainsString(t, want)
+					}
+				})
+
+				t.Run("HEAD", func(t *testing.T) {
+					res := client.Head(ep)
+
+					verifyMeta(t, res)
+					res.AssertBodyEmpty(t)
+				})
+			})
+		}
+	}
+}

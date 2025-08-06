28dac2e3615ee55ac3e8cae0062b07f6e0c2170f
Sam <sktylr@gmail.com>
2025-08-06 22:02:21 +0100

    add backend server setup + fix deps

--- examples/todo/backend.go
+++ examples/todo/backend.go
@@ -4,8 +4,21 @@ import (
 	"database/sql"
 
 	"github.com/sktylr/routeit"
+	"github.com/sktylr/routeit/examples/todo/db"
+	"github.com/sktylr/routeit/examples/todo/handlers"
 )
 
-func GetBackendServer(db *sql.DB) *routeit.Server {
-	return routeit.NewServer(routeit.ServerConfig{})
+func GetBackendServer(dbConn *sql.DB) *routeit.Server {
+	usersRepo := db.NewUsersRepository(dbConn)
+	srv := routeit.NewServer(routeit.ServerConfig{
+		Debug:                  false,
+		StrictClientAcceptance: true,
+		AllowedHosts:           []string{".localhost"},
+	})
+	srv.RegisterMiddleware(routeit.CorsMiddleware(routeit.DefaultCors()))
+	srv.RegisterRoutes(routeit.RouteRegistry{
+		"/auth/register": handlers.RegisterUserHandler(usersRepo),
+		"/auth/login":    handlers.LoginHandler(usersRepo),
+	})
+	return srv
 }

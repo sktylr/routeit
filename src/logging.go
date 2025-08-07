3679aa43341e449cf7e2e19f927813f8460344f9
Sam <sktylr@gmail.com>
2025-08-07 21:35:52 +0100

    add custom log attributes for each req

--- src/logging.go
+++ src/logging.go
@@ -6,11 +6,19 @@ import (
 	"os"
 )
 
+// The [LogAttrExtractor] can be used to output additional request metadata to
+// the default log line that is produced on every request. By default, routeit
+// will log the request path (including the rewritten and edge path), the HTTP
+// status of the response, the request's User-Agent field, the request's method
+// and the client's IP address.
+type LogAttrExtractor func(*Request, HttpStatus) []slog.Attr
+
 type logger struct {
-	log *slog.Logger
+	log        *slog.Logger
+	extraAttrs LogAttrExtractor
 }
 
-func newLogger(handler slog.Handler, debug bool) *logger {
+func newLogger(handler slog.Handler, debug bool, fn LogAttrExtractor) *logger {
 	if handler != nil {
 		return &logger{log: slog.New(handler)}
 	}
@@ -21,7 +29,10 @@ func newLogger(handler slog.Handler, debug bool) *logger {
 		logOpts.Level = slog.LevelInfo
 	}
 	jsonHandler := slog.NewJSONHandler(os.Stdout, &logOpts)
-	return &logger{log: slog.New(jsonHandler)}
+	if fn == nil {
+		fn = func(r *Request, hs HttpStatus) []slog.Attr { return []slog.Attr{} }
+	}
+	return &logger{log: slog.New(jsonHandler), extraAttrs: fn}
 }
 
 func (l *logger) LogRequestAndResponse(rw *ResponseWriter, req *Request) {
@@ -35,17 +46,7 @@ func (l *logger) LogRequestAndResponse(rw *ResponseWriter, req *Request) {
 		level = slog.LevelInfo
 	}
 
-	attrs := []slog.Attr{
-		slog.String("method", req.mthd.name),
-		slog.String("path", req.Path()),
-		slog.String("edge_path", req.uri.EdgePathString()),
-		slog.String("raw_path", req.RawPath()),
-		slog.Int("status", int(rw.s.code)),
-		slog.String("user_agent", req.userAgent),
-		slog.String("client_ip", req.ip),
-	}
-
-	l.log.LogAttrs(req.Context(), level, "Received request", attrs...)
+	l.log.LogAttrs(req.Context(), level, "Received request", l.attrs(rw, req)...)
 
 	if rw.s.isError() && len(req.body) != 0 {
 		l.log.Debug("Request failed", slog.String("body", string(req.body)))
@@ -70,4 +71,17 @@ func (l *logger) Error(msg string, args ...any) {
 
 func (l *logger) Enabled(ctx context.Context, level slog.Level) bool {
 	return l.log.Enabled(ctx, level)
+}
+
+func (l *logger) attrs(rw *ResponseWriter, req *Request) []slog.Attr {
+	base := []slog.Attr{
+		slog.String("method", req.mthd.name),
+		slog.String("path", req.Path()),
+		slog.String("edge_path", req.uri.EdgePathString()),
+		slog.String("raw_path", req.RawPath()),
+		slog.Int("status", int(rw.s.code)),
+		slog.String("user_agent", req.userAgent),
+		slog.String("client_ip", req.ip),
+	}
+	return append(base, l.extraAttrs(req, rw.s)...)
 }

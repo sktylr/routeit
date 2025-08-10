4853d5422767a67cda94976b13c5bcd9246061c5
Sam <sktylr@gmail.com>
2025-08-10 19:55:59 +0100

    add simple "mark as completed" method

--- examples/todo/db/items.go
+++ examples/todo/db/items.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type TodoItemRepository struct {
 	db *sql.DB
@@ -8,4 +12,15 @@ type TodoItemRepository struct {
 
 func NewTodoItemRepository(db *sql.DB) *TodoItemRepository {
 	return &TodoItemRepository{db: db}
+}
+
+func (r *TodoItemRepository) MarkAsCompleted(ctx context.Context, id string) error {
+	query := `
+		UPDATE items
+		SET status = 'COMPLETED',
+			updated = ?
+		WHERE id = ?
+	`
+	_, err := r.db.ExecContext(ctx, query, time.Now(), id)
+	return err
 }

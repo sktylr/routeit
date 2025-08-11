17769af51941bf7451fb96485a0d25c87102d6ad
Sam <sktylr@gmail.com>
2025-08-11 13:12:19 +0100

    add lookup (truncated) by user

--- examples/todo/dao/list.go
+++ examples/todo/dao/list.go
@@ -7,3 +7,9 @@ type TodoList struct {
 	Description string
 	Items       []TodoItem
 }
+
+type AggregateTodoList struct {
+	TodoList
+	TotalItems     int
+	CompletedItems int
+}
--- examples/todo/db/lists.go
+++ examples/todo/db/lists.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -82,6 +84,94 @@ func (r *TodoListRepository) UpdateList(ctx context.Context, listId, name, descr
 	}
 
 	return list, nil
+}
+
+func (r *TodoListRepository) GetListsByUser(ctx context.Context, userId string, page, pageSize int) ([]*dao.AggregateTodoList, error) {
+	offset := (page - 1) * pageSize
+	listQuery := `
+		SELECT l.id, l.created, l.updated, l.user_id, l.name, l.description,
+		       COUNT(i.id) AS total_items,
+		       SUM(CASE WHEN i.status = 'COMPLETED' THEN 1 ELSE 0 END) AS completed_items
+		FROM lists l
+		LEFT JOIN items i ON l.id = i.list_id
+		WHERE l.user_id = ?
+		GROUP BY l.id
+		ORDER BY l.created DESC
+		LIMIT ? OFFSET ?
+	`
+	rows, err := r.db.QueryContext(ctx, listQuery, userId, pageSize, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	lists := []*dao.AggregateTodoList{}
+	listIds := []any{}
+	for rows.Next() {
+		var l dao.AggregateTodoList
+		if err := rows.Scan(&l.Id, &l.Created, &l.Updated, &l.UserId, &l.Name, &l.Description, &l.TotalItems, &l.CompletedItems); err != nil {
+			return nil, err
+		}
+		l.Items = []dao.TodoItem{}
+		listIds = append(listIds, l.Id)
+		lists = append(lists, &l)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	if len(lists) == 0 {
+		return lists, nil
+	}
+
+	itemsByList, err := r.getListItems(ctx, listIds)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range lists {
+		id := lists[i].Id
+		lists[i].Items = itemsByList[id]
+	}
+	return lists, nil
+}
+
+func (r *TodoListRepository) getListItems(ctx context.Context, ids []any) (map[string][]dao.TodoItem, error) {
+	placeholder := strings.Join(slices.Repeat([]string{"?"}, len(ids)), ",")
+	query := fmt.Sprintf(`
+		SELECT id, created, updated, user_id, list_id, name, status
+		FROM items
+		WHERE list_id IN (%s)
+		ORDER BY list_id, created ASC
+	`, placeholder)
+	rows, err := r.db.QueryContext(ctx, query, ids...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	itemsByList := make(map[string][]dao.TodoItem)
+	for rows.Next() {
+		var item dao.TodoItem
+		if err := rows.Scan(
+			&item.Id,
+			&item.Created,
+			&item.Updated,
+			&item.UserId,
+			&item.TodoListId,
+			&item.Name,
+			&item.Status,
+		); err != nil {
+			return nil, err
+		}
+		if len(itemsByList[item.TodoListId]) < 10 {
+			itemsByList[item.TodoListId] = append(itemsByList[item.TodoListId], item)
+		}
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return itemsByList, nil
 }
 
 func (e *ErrListNotFound) Error() string {

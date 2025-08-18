5bbbd9a71e7a7028897f87ed1ca90642293239dd
Sam <sktylr@gmail.com>
2025-08-18 19:51:51 +0100

    move `queryParamOrDefault` to shared utilities

--- examples/todo/handlers/lists.go
+++ examples/todo/handlers/lists.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"strconv"
 	"time"
 
 	"github.com/sktylr/routeit"
@@ -57,24 +56,6 @@ type listResponse struct {
 }
 
 func ListsMultiHandler(repo *db.TodoListRepository) routeit.Handler {
-	queryParamOrDefault := func(param string, qs *routeit.QueryParams, def int) (int, error) {
-		val, hasVal, err := qs.Only(param)
-		if err != nil {
-			return 0, err
-		}
-
-		valNum := def
-		if hasVal {
-			valNum, err = strconv.Atoi(val)
-			if err != nil || valNum <= 0 {
-				return 0, routeit.ErrBadRequest().
-					WithMessagef("%#q is not a valid %s number", param, val).
-					WithCause(err)
-			}
-		}
-		return valNum, nil
-	}
-
 	return routeit.MultiMethod(routeit.MultiMethodHandler{
 		Get: func(rw *routeit.ResponseWriter, req *routeit.Request) error {
 			userId, hasUser := userIdFromRequest(req)
--- examples/todo/handlers/util.go
+++ examples/todo/handlers/util.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/sktylr/routeit"
 	"github.com/sktylr/routeit/examples/todo/dao"
 )
@@ -12,4 +14,22 @@ func userIdFromRequest(req *routeit.Request) (string, bool) {
 		return "", false
 	}
 	return user.Id, true
+}
+
+func queryParamOrDefault(param string, qs *routeit.QueryParams, def int) (int, error) {
+	val, hasVal, err := qs.Only(param)
+	if err != nil {
+		return 0, err
+	}
+
+	valNum := def
+	if hasVal {
+		valNum, err = strconv.Atoi(val)
+		if err != nil || valNum <= 0 {
+			return 0, routeit.ErrBadRequest().
+				WithMessagef("%#q is not a valid %s number", param, val).
+				WithCause(err)
+		}
+	}
+	return valNum, nil
 }

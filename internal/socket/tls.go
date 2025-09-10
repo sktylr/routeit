1c719b8b14207c4856262a8bd82322623f726c11
Sam <sktylr@gmail.com>
2025-09-10 11:30:18 +0100

    [https] TLS over TCP socket (#40)
    ### Summary
    <!-- A high level summary of the changes, including any gotchas that the
    reviewer should watch out for when reviewing. -->
    
    Following on from the previous PR, this PR adds a concrete TLS over TCP
    implementation of the `Socket` interface. This uses the `crypto/tls`
    socket and is similar in most implementation details to the existing TCP
    socket.
    
    ### Motivation
    <!-- Why is this change necessary? This can be a link to a GitHub issue
    or reproduction steps for a bug etc. -->
    
    In order to support HTTPS, we need to be able to support responding to
    TLS connections, which also includes the TLS handshake. Once decrypted,
    the message should be a HTTP/1.1 message, so can be reacted to in the
    same way the server normally would.
    
    ### Test plan
    <!-- How did you test the changes? Are there edge cases you are missing
    or were unable to test? -->
    
    - [x] Unit tests

---
+++ internal/socket/tls.go
@@ -0,0 +1,55 @@
+package socket
+
+import (
+	ctls "crypto/tls"
+	"fmt"
+	"net"
+	"sync/atomic"
+)
+
+type tls struct {
+	port   uint16
+	conf   *ctls.Config
+	ln     net.Listener
+	closed atomic.Bool
+}
+
+// Creates a new socket that uses TLS over TCP with the given config and port.
+func NewTlsSocket(port uint16, conf *ctls.Config) Socket {
+	return &tls{port: port, conf: conf.Clone()}
+}
+
+func (t *tls) Bind() error {
+	ln, err := ctls.Listen("tcp", fmt.Sprintf(":%d", t.port), t.conf)
+	if err != nil {
+		return err
+	}
+	t.ln = ln
+	t.closed.Store(false)
+	return nil
+}
+
+func (t *tls) Serve(onConn onConnection, onErr onError) {
+	defer t.Close()
+
+	for {
+		if t.closed.Load() {
+			return
+		}
+
+		conn, err := t.ln.Accept()
+		if err != nil {
+			go onErr(err)
+			continue
+		}
+		go onConn(conn)
+	}
+}
+
+func (t *tls) Close() error {
+	if t.ln == nil {
+		return ErrSocketNotInUse
+	}
+	t.closed.Store(true)
+	return t.ln.Close()
+}
---
+++ internal/socket/tls_test.go
@@ -0,0 +1,114 @@
+package socket
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	ctls "crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"net"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestTlsSocketListenAndServe(t *testing.T) {
+	conf := newTestTLSConfig()
+	s := NewTlsSocket(0, conf).(*tls)
+
+	if err := s.Bind(); err != nil {
+		t.Fatalf("Bind failed: %v", err)
+	}
+	defer s.ln.Close()
+
+	called := make(chan struct{})
+
+	go s.Serve(
+		func(conn net.Conn) {
+			defer conn.Close()
+			if _, err := conn.Write([]byte("OK")); err != nil {
+				t.Errorf("failed to write to connection: %+v", err)
+			}
+			close(called)
+		}, func(err error) {
+			if strings.Contains(err.Error(), "use of closed network connection") {
+				return
+			}
+			t.Errorf("unexpected error: %v", err)
+		},
+	)
+
+	addr := s.ln.Addr().String()
+	clientConf := conf.Clone()
+	clientConf.InsecureSkipVerify = true
+	conn, err := ctls.Dial("tcp", addr, clientConf)
+	if err != nil {
+		t.Fatalf("failed to dial TLS server: %v", err)
+	}
+	conn.Close()
+
+	select {
+	case <-called:
+	case <-time.After(time.Second):
+		t.Fatal("consumer was not called in time")
+	}
+}
+
+func TestTlsSocketClose(t *testing.T) {
+	tests := []struct {
+		name    string
+		setup   func(Socket)
+		wantErr bool
+	}{
+		{
+			name: "success",
+			setup: func(s Socket) {
+				if err := s.Bind(); err != nil {
+					t.Fatalf("unexpected error while binding: %+v", err)
+				}
+			},
+		},
+		{
+			name:    "socket not bound",
+			setup:   func(s Socket) {},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			s := NewTlsSocket(0, newTestTLSConfig())
+			tc.setup(s)
+
+			err := s.Close()
+
+			if err != nil && !tc.wantErr {
+				t.Errorf("unexpected error: %+v", err)
+			}
+			if err == nil && tc.wantErr {
+				t.Error("wanted error but not found")
+			}
+		})
+	}
+}
+
+// newTestTLSConfig returns a minimal TLS config with an ephemeral self-signed
+// cert. Suitable for tests only.
+func newTestTLSConfig() *ctls.Config {
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject:      pkix.Name{CommonName: "localhost"},
+		NotBefore:    time.Now().Add(-time.Hour),
+		NotAfter:     time.Now().Add(time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	der, _ := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	cert := ctls.Certificate{Certificate: [][]byte{der}, PrivateKey: priv}
+
+	return &ctls.Config{Certificates: []ctls.Certificate{cert}, InsecureSkipVerify: true}
+}
